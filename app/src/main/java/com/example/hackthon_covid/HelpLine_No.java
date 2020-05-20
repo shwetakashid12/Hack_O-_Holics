28c893a8f3f240553500ace61bf53fca966e6721
imdeva <devanagarkar4@gmail.com>
2020-05-20 20:25:12 +0530

    New file added

---
+++ app/src/main/java/com/example/hackthon_covid/HelpLine_No.java
@@ -0,0 +1,14 @@
+package com.example.hackthon_covid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HelpLine_No extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help_line__no);
+    }
+}
